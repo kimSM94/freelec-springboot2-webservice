581ac3de2a5ecc16cc0bfb967e71993dc3b1547d
kimSM94 <94KimSM@gmail.com>
2023-09-20 10:35:58 +0900

    Security Oauth 인증 Test Controller

--- src/main/java/com/jojoldu/book/springboot/Application.java
+++ src/main/java/com/jojoldu/book/springboot/Application.java
@@ -3,7 +3,6 @@ package com.jojoldu.book.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 // 스프링부투의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정 , 특히나 @SpringBootAppliaction이 있는 위치부터 설정
 // 을 읽어가기 때문에 이 클래스는 항상 프로그젝의 최상단에 위치해야 합니다.
@@ -16,7 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  *  WHY > 언제 어디서나 같은 환경에서 스프링 부트를 배포할 수 있기 때문에
  */
 
-@EnableJpaAuditing
+//@EnableJpaAuditing  // TEST할때는 WwebMvcTest이다보니 없음
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
